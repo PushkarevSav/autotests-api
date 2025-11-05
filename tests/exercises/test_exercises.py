28b6a66ea49bec5d876ccf076359d61adcfec117
PushkarevSav <pespizdec@yandex.ru>
2025-11-05 18:44:52 +0600

    9.4 Практикуемся в написании API автотестов. Часть 3 Удаление задания

--- tests/exercises/test_exercises.py
+++ tests/exercises/test_exercises.py
@@ -2,6 +2,7 @@ from http import HTTPStatus
 
 import pytest
 
+from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_client import ExercisesClient
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
     GetExercisesQuerySchema, GetExercisesResponseSchema, GetExerciseResponseSchema, UpdateExerciseRequestSchema, \
@@ -12,13 +13,13 @@ from fixtures.files import FileFixture
 from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.exercises import assert_create_exercise_response, assert_get_exercise_response, \
-    assert_update_exercise_response
+    assert_update_exercise_response, assert_exercise_not_found_response
 from tools.assertions.schema import validate_json_schema
 
 
 @pytest.mark.regression
 @pytest.mark.exercises
-class TestExercises():
+class TestExercises:
 
     def test_create_exercises(self, function_user: UserFixture, function_course: CourseFixture,
                               function_file: FileFixture, exercise_client: ExercisesClient):
@@ -51,3 +52,21 @@ class TestExercises():
         assert_update_exercise_response(request, response_data)
 
         validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_delete_exercise(self,function_exercise: ExerciseFixture, exercise_client: ExercisesClient):
+
+        response = exercise_client.delete_exercise_api(exercise_id= function_exercise.response.exercise.id)
+        assert_status_code(response.status_code, HTTPStatus.OK)
+
+        get_response = exercise_client.get_exercise_api(exercise_id=function_exercise.response.exercise.id)
+        data_response = InternalErrorResponseSchema.model_validate_json(get_response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.NOT_FOUND)
+
+        assert_exercise_not_found_response(data_response)
+
+        validate_json_schema(get_response.json(), data_response.model_json_schema())
+
+
+
+
--- tools/assertions/exercises.py
+++ tools/assertions/exercises.py
@@ -1,7 +1,9 @@
+from clients.errors_schema import InternalErrorResponseSchema
 from clients.exercises.exercises_schema import CreateExerciseRequestSchema, CreateExerciseResponseSchema, \
     ExerciseSchema, GetExerciseResponseSchema, GetExercisesResponseSchema, UpdateExerciseResponseSchema, \
     UpdateExerciseRequestSchema
 from tools.assertions.base import assert_status_code, assert_equal, assert_length
+from tools.assertions.errors import assert_internal_error_response
 
 
 def assert_create_exercise_response(actual: CreateExerciseRequestSchema, expected: CreateExerciseResponseSchema):
@@ -69,3 +71,16 @@ def assert_update_exercise_response(actual: UpdateExerciseRequestSchema, expecte
         assert_equal(actual.description, expected.exercise.description, 'description')
     if actual.estimated_time is not None:
         assert_equal(actual.estimated_time, expected.exercise.estimated_time, 'estimated_time')
+
+
+def assert_exercise_not_found_response(actual: InternalErrorResponseSchema):
+        """
+        Функция для проверки ошибки, если файл не найден на сервере.
+
+        :param actual: Фактический ответ.
+        :raises AssertionError: Если фактический ответ не соответствует ошибке "File not found"
+        """
+        # Ожидаемое сообщение об ошибке, если файл не найден
+        expected = InternalErrorResponseSchema(details="Exercise not found")
+        # Используем ранее созданную функцию для проверки внутренней ошибки
+        assert_internal_error_response(actual, expected)
